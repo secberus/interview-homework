60806546d4c23adafe0902b40a576d43652ae574
Sigkar <duncan@duncanpierce.com>
2020-05-15 18:25:57 -0400

    Added dark theme

--- packages/interview-homework/src/redux/reducers/ui.js
+++ packages/interview-homework/src/redux/reducers/ui.js
@@ -2,16 +2,19 @@
  * @file reducers/ui.js
  */
 import produce from 'immer';
-
+import {UIActions} from '../actions/ui'
 const initialState = {
-  oranges: []
+  useLightMode: false
 }
 
 const UIReducer = produce((state = initialState, action) => {
   switch(action.type){
+    case UIActions.SET_ATTRIBUTE:
+      state[action.key] = action.value;
+      break;
     default:
-      return state;
   }
+  return state;
 })
 
 export default UIReducer;
---
+++ packages/interview-homework/src/screens/home/Styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components/macro';
+
+export const TextContainer = styled.h2`
+  text-align: center;
+`;
